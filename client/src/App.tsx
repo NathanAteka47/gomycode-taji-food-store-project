18b24a2e4c2ea626738202ec68deb518e8b2a022
Nathan Ateka <nathanateka81@gmail.com>
2025-06-25 08:33:26 +0300

    added client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      <h1>Taji Food Store</h1>
+      </div>
+      <p className="read-the-docs">
+        Traditional Meals
+      </p>
+      <div>
+      <img src="/logo.png" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
